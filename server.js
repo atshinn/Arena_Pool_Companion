bb7e103e47100074037f2dd95b515c033c2c0c16
atshinn <atshinn@asu.edu>
2017-10-09 17:28:56 -0700

    Got style sheet connected, and images are showing up

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 console.log(__dirname);
-app.set('views',__dirname + '\\views');
-app.set('view engine','pug');
 
+app.set('views',path.join(__dirname,"views"));
+app.set('view engine','pug');
+app.use(express.static("public"));
 app.listen(8000,'localhost');
 
 
